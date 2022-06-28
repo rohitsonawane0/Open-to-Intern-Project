bee16210be6b08ac43a6e13acada4816572e1e5b
Rohit Sonawane <rrsonawne@gmail.com>
2022-06-28 17:04:00 +0530

    intital push

--- src/route/route.js
+++ src/route/route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const route = express.Router();
 
-route.get("/", function (res, req) {
-  res.status(200).send("It's working");
+route.get("/", function (req, res) {
+  res.status(200).send("testing api");
 });
 
-module.exports = router;
+module.exports = route;
